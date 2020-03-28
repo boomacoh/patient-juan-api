fc92dddd17358dfcb9c5d938e64b8b1f52b29c2e
Anthony <anthonyballugjr@gmail.com>
2020-03-28 21:50:15 +0800

    FIX: path.resolve returns undefined part of image path

--- src/utility/image-uploader/upload/index.js
+++ src/utility/image-uploader/upload/index.js
@@ -1,29 +1,25 @@
 const sharp = require('sharp');
 const path = require('path');
 
-// const x = path.join(__dirname, '../../../public/images/profile/');
-// console.log(x);
-// console.log(path.resolve(x));
-
 class UploadImage {
 
   constructor(imageFile) {
     this.imageFile = imageFile;
-    this.imageDirectory = path.join(__dirname, '../../../public/images/profile/');
   }
 
   saveImage() {
     const filename = UploadImage.createFileName();
     const filepath = UploadImage.path(filename);
-    console.log('FILE PATH: ', filepath);
 
-    sharp(this.imageFile.buffer)
+    return sharp(this.imageFile.buffer)
       .resize(300, 300, { fit: sharp.fit.inside, withoutEnlargement: true })
-      .toFile(this.imageDirectory + filename)
-      .then(success => console.log(success))
-      .catch(error => console.log(error));
-
-    return filename;
+      .toFile(filepath)
+      .then(success => {
+        return { filename: filename, message: success };
+      })
+      .catch(error => {
+        return error;
+      });
   }
 
   static createFileName() {
@@ -31,7 +27,7 @@ class UploadImage {
   }
 
   static path(filename) {
-    return path.resolve(`${this.imageDirectory}/${filename}`);
+    return path.resolve(__dirname, '../../../public/images/profile/', filename);
   }
 
 }
