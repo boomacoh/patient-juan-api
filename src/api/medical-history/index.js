96bb47bb92775b99b8b407c5bef57bbbbdc22169
Anthony <anthonyballugjr@gmail.com>
2020-04-22 20:46:45 +0800

    IMPROVE: Add Sub-histories crud routes (Surgery, Allergy, Substance, Medication)

--- src/api/medical-history/index.js
+++ src/api/medical-history/index.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
 const controller = require('./medical-history.controller');
+const { jwtAuth, checkPermission } = require('../../services/auth/jwt');
 
 router.get('/', controller.getAll);
-router.get('/:medicalHistoryId', controller.getOne);
-router.get('/patient/:patientId', controller.getPatientMedicalHistory);
-router.get('/test/test/test', controller.updatePastMedicalHistory);
+router.get('/:medicalHistoryId', jwtAuth.required, controller.getOne);
+router.get('/patient/:patientId', jwtAuth.required, controller.getPatientMedicalHistory);
+
+router.post('/surgery', jwtAuth.required, controller.addSurgery);
+router.post('/medication', jwtAuth.required, controller.addMedication);
+router.post('/allergy', jwtAuth.required, controller.addAllergy);
+router.post('/substance', jwtAuth.required, controller.addSubstance);
+
+router.put('/surgery/:id', jwtAuth.required, controller.updateSurgery);
+router.put('/medication/:id', jwtAuth.required, controller.updateMedication);
+router.put('/allergy/:id', jwtAuth.required, controller.updateAllergy);
+router.put('/substance/:id', jwtAuth.required, controller.updateSubstance);
+
+router.delete('/surgery/:id', jwtAuth.required, controller.deleteSurgery);
+router.delete('/medication/:id', jwtAuth.required, controller.deleteMedication);
+router.delete('/allergy/:id', jwtAuth.required, controller.deleteAllergy);
+router.delete('/substance/:id', jwtAuth.required, controller.deleteSubstance);
 
 module.exports = router;
