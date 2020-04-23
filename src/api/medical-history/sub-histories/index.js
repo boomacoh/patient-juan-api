26b7e6612247d62c9b2cb85c8bf56c9ad4dd8506
Anthony <anthonyballugjr@gmail.com>
2020-04-23 17:40:19 +0800

    IMPROVE: Rename Illness -> PastIllnesses, Add setter and getter methods

--- src/api/medical-history/sub-histories/index.js
+++ src/api/medical-history/sub-histories/index.js
@@ -5,10 +5,36 @@ const Allergy = sequelize.define('allergy', {
   remarks: Sequelize.STRING
 });
 
-const Illness = sequelize.define('illness', {
-  illness: Sequelize.STRING,
-  remarks: Sequelize.STRING,
+const PastIllness = sequelize.define('pastIllness', {
+  illness: Sequelize.STRING(500),
+  remarks: Sequelize.STRING(500),
   parent: Sequelize.STRING
+}, {
+  scopes: {
+    parent(parent) { return { where: { parent: parent } } }
+  },
+  setterMethods: {
+    illness(value) {
+      if (value) return this.setDataValue('illness', value.join('-'));
+      this.setDataValue('illness', null)
+    },
+    remarks(value) {
+      if (value) return this.setDataValue('remarks', value.join('-'));
+      this.setDataValue('remarks', null);
+    }
+  },
+  getterMethods: {
+    illness() {
+      const illnesses = this.getDataValue('illness').split('-');
+      if (illnesses) return illnesses;
+      return [];
+    },
+    remarks(){
+      const remarks = this.getDataValue('remarks').split('-');
+      if(remarks) return remarks;
+      return [];
+    }
+  }
 });
 
 const Surgery = sequelize.define('surgery', {
@@ -38,4 +64,4 @@ const Substance = sequelize.define('substance', {
 // Medication.removeAttribute('id');
 // Substance.removeAttribute('id');
 
-module.exports = { Allergy, Medication, Surgery, Illness, Substance }
+module.exports = { Allergy, Medication, Surgery, PastIllness, Substance }
