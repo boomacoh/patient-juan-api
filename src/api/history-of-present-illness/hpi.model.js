e2271f95f8fd3a4328d48b8c2cdaeaf7f6b9c14b
Anthony <anthonyballugjr@gmail.com>
2020-04-29 19:31:45 +0800

    IMPROVE: Remove explicit primaryKey

--- src/api/history-of-present-illness/hpi.model.js
+++ src/api/history-of-present-illness/hpi.model.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 
 const Hpi = sequelize.define('hpi', {
-  hpiId: { type: Sequelize.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
   timeFrame: { type: Sequelize.STRING },
   details: { type: Sequelize.STRING }
 }, {
