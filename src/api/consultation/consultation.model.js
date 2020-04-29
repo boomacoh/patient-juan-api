849982717765332d832d7a94d67733cc2812e2e1
Anthony <anthonyballugjr@gmail.com>
2020-04-29 19:37:25 +0800

    IMPROVE: Make ChiefComplaint model as field in Consultation, Drop/Remove ChiefComplaint model, Add Institution association to Consultation

--- src/api/consultation/consultation.model.js
+++ src/api/consultation/consultation.model.js
@@ -1,21 +1,19 @@
 const Sequelize = require('sequelize');
-const ChiefComplaint = require('../chief-complaint/chief-complaint.model');
 const Hpi = require('../history-of-present-illness/hpi.model');
 const Patient = require('../patient/patient.model');
 const User = require('../user/user/user.model');
+const Institution = require('../institution/institution.model');
 const { Ros_CardioVascularSystem, Ros_GastroIntestinalSystem, Ros_GeneralHealth, Ros_Heent, Ros_NervousSystem, Ros_RespiratorySystem } = require('../review-of-systems/review-of-systems.models');
 
 const Consultation = sequelize.define('consultation', {
     consultationId: { type: Sequelize.INTEGER(11), allowNull: false, primaryKey: true, autoIncrement: true },
+    chiefComplaint: { type: Sequelize.STRING, allownNull: false },
     queueId: { type: Sequelize.STRING }
 }, {
     defaultScope: {
         include: [{ all: true, attributes: { exclude: ['createdAt', 'updatedAt', 'consultationId'] } }]
     }
 });
-
-Consultation.hasOne(ChiefComplaint, { foreignKey: 'consultationId' });
-ChiefComplaint.belongsTo(Consultation, { foreignKey: 'consultationId' });
 
 Consultation.hasMany(Hpi, { foreignKey: 'consultationId' });
 Hpi.belongsTo(Consultation, { foreignKey: 'consultationId' });
@@ -25,6 +23,9 @@ Patient.hasMany(Consultation, { foreignKey: 'patientId' });
 
 Consultation.belongsTo(User, { as: 'physician', foreignKey: 'physicianId' });
 User.hasMany(Consultation, { as: 'physician', foreignKey: 'physicianId' });
+
+Consultation.belongsTo(Institution, { foreignKey: 'institutionId' });
+Institution.hasMany(Consultation, { foreignKey: 'institutionId' });
 
 Consultation.hasOne(Ros_GeneralHealth, { as: 'rosGeneralHealth', foreignKey: 'consultationId' });
 Consultation.hasOne(Ros_Heent, { as: 'rosHeent', foreignKey: 'consultationId' });
