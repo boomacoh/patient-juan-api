e515d677c2bcbce297c65e7b4bf50a00d33110ec
Anthony <anthonyballugjr@gmail.com>
2020-05-01 19:11:53 +0800

    IMPROVE: Add associations (Vital Signs, Antrophometric data)

--- src/api/physical-exam/physical-exam.model.js
+++ src/api/physical-exam/physical-exam.model.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const VitalSigns = require('./vital-signs.model');
+const AntrophometricData = require('./antrophometric-data.model');
 
 const PhysicalExam = sequelize.define('physicalExam', {
   generalSurvey: Sequelize.STRING,
@@ -10,7 +12,11 @@ const PhysicalExam = sequelize.define('physicalExam', {
   nervousSystem: Sequelize.STRING,
   rectalExamination: Sequelize.STRING,
   pelvicExamination: Sequelize.STRING
-})
+}, {
+  defaultScope: { include: ['vitalSigns', 'antrophometricData'] }
+});
 
+PhysicalExam.hasOne(VitalSigns, { as: 'vitalSigns', foreignKey: 'physicalExamId' });
+PhysicalExam.hasOne(AntrophometricData, { as: 'antrophometricData', foreignKey: 'physicalExamId' });
 
 module.exports = PhysicalExam;
