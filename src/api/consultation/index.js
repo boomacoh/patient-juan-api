056bf9a862357d7120a20ffd05c07b8dba8323ac
Anthony <anthonyballugjr@gmail.com>
2020-05-04 18:42:23 +0800

    IMPROVE: Add updatePhysicalExam method gateway/route

--- src/api/consultation/index.js
+++ src/api/consultation/index.js
@@ -7,12 +7,13 @@ router.get('/:consultationId', jwtAuth.required, controller.getOne);
 router.get('/find/options', jwtAuth.required, controller.find);
 router.get('/association/:consultationId/:association', controller.getAssociation);
 
+router.put('/:consultationId', jwtAuth.required, controller.update);
 
 router.post('/', jwtAuth.required, controller.create);
 router.post('/hpis/:consultationId', controller.updateHpis);
 router.post('/rosys/:consultationId', jwtAuth.required, controller.updateRosys);
 router.post('/rosys/:group/:consultationId', jwtAuth.required, controller.updateRosysGroup);
-// router.post('/physical-exam/:consultationId')
+router.post('/physical-exam/:consultationId', jwtAuth.required, controller.updatePhysicalExam);
 
 
 module.exports = router;
