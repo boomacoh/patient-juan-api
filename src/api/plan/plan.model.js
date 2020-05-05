39b12e49c5201ea87d00680f630e0b755a07e4ce
Anthony <anthonyballugjr@gmail.com>
2020-05-05 18:48:13 +0800

    IMPROVE: Add associations (Drug, Diagnostic)

--- src/api/plan/plan.model.js
+++ src/api/plan/plan.model.js
@@ -1,8 +1,15 @@
 const Sequelize = require('sequelize');
+const Drug = require('./drug.model');
+const Diagnostic = require('./diagnostic.model');
 
 const Plan = sequelize.define('plan', {
   diet: Sequelize.STRING,
   disposition: Sequelize.STRING
+}, {
+  defaultScope: { include: [Drug, Diagnostic] }
 });
+
+Plan.hasMany(Drug);
+Plan.hasMany(Diagnostic);
 
 module.exports = Plan;
