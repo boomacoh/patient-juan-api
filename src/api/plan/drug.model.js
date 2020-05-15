d003b0f2507970d18c9b4c196fdb02a1bcca2ff4
Anthony <anthonyballugjr@gmail.com>
2020-05-15 20:15:08 +0800

    FIX: mispelled attribute

--- src/api/plan/drug.model.js
+++ src/api/plan/drug.model.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const Drug = sequelize.define('drug', {
   generic: Sequelize.STRING,
   brand: Sequelize.STRING,
-  prepration: Sequelize.STRING,
+  preparation: Sequelize.STRING,
   frequency: Sequelize.STRING,
   route: Sequelize.STRING,
   purpose: Sequelize.STRING
