25b38d9a587b93ff87f19f76fb1ebce3dafd21c5
Anthony <anthonyballugjr@gmail.com>
2020-05-22 16:56:21 +0800

    FIX: Undefined id error

--- src/api/user-institution/user-institution.model.js
+++ src/api/user-institution/user-institution.model.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 
 const UserInstitution = sequelize.define('user_institution', {
   assignmentId: { type: Sequelize.UUID, allowNull: false, primaryKey: true, defaultValue: Sequelize.UUIDV4 },
-  userId: { type: Sequelize.INTEGER(11), allowNull: false },
-  institutionId: { type: Sequelize.INTEGER(11), allowNull: false },
+  userId: { type: Sequelize.UUID, allowNull: false },
+  institutionId: { type: Sequelize.UUID, allowNull: false },
   isDefault: { type: Sequelize.BOOLEAN, defaultValue: false, allowNull: false },
   access: { type: Sequelize.STRING(255), allowNull: false }
 }, {
