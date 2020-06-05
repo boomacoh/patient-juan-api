ec364a4f682b60c2abb351d2ca41a3f38cd376bd
Anthony <anthonyballugjr@gmail.com>
2020-06-05 20:40:10 +0800

    IMPROVE: Update route naming

--- src/api/consultation/index.js
+++ src/api/consultation/index.js
@@ -4,19 +4,15 @@ const { jwtAuth, checkPermission } = require('../../services/auth/jwt');
 
 router.get('/', jwtAuth.required, controller.getAll);
 router.get('/:id', jwtAuth.required, controller.getOne);
-router.get('/history/history', controller.getHistory);
-router.get('/find/options', jwtAuth.required, controller.find);
-router.get('/association/:id/:association', controller.getAssociation);
-router.get('/billing/:id', jwtAuth.required, controller.getBilling);
+router.get('/:id/billing', jwtAuth.required, controller.getBilling);
 
 router.put('/:id', jwtAuth.required, controller.update);
 
 router.post('/', jwtAuth.required, controller.create);
-router.post('/hpis/:id', controller.updateHpis);
-router.post('/rosys/:id', jwtAuth.required, controller.updateRosys);
-router.post('/rosys/:group/:id', jwtAuth.required, controller.updateRosysGroup);
-router.post('/physical-exam/:id', jwtAuth.required, controller.updatePhysicalExam);
-router.post('/plan/:id', jwtAuth.required, controller.updateplan);
-
+router.post('/:id/hpis', controller.updateHpis);
+router.post('/:id/rosys', jwtAuth.required, controller.updateRosys);
+router.post('/:id/rosys/:group', jwtAuth.required, controller.updateRosysGroup);
+router.post('/:id:/physical-exam', jwtAuth.required, controller.updatePhysicalExam);
+router.post('/:id:/plan/', jwtAuth.required, controller.updateplan);
 
 module.exports = router;
