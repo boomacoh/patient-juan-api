be4615da54178be98a8e8f334bb8aa9d41171dd6
Anthony <anthonyballugjr@gmail.com>
2020-06-05 20:41:07 +0800

    IMPROVE: Add default scope

--- src/api/billing/billing-items.model.js
+++ src/api/billing/billing-items.model.js
@@ -3,21 +3,22 @@ const Datatypes = require('sequelize');
 const BillingItems = sequelize.define('billing_items', {
   name: { type: Datatypes.STRING, allowNull: false },
   qty: { type: Datatypes.INTEGER, allowNull: false },
-  discount: Datatypes.INTEGER,
+  discount: Datatypes.STRING,
   discountType: Datatypes.STRING,
+  discountValue: {type: Datatypes.FLOAT, defaultValue: 0},
   hmo: { type: Datatypes.BOOLEAN, defaultValue: false },
   price: { type: Datatypes.FLOAT, allowNull: false },
   total: { type: Datatypes.VIRTUAL(Datatypes.FLOAT) }
 }, {
   getterMethods: {
     total() {
-      const discount = this.getDataValue('discount')
-      const a = this.price * this.qty;
+      const discount = this.getDataValue('discount');
+      const totalValue = this.price * this.qty;
       if (discount) {
-        if (this.discountType === 'amt') return a - this.discount;
-        if (this.discountType === 'percent') return a - (a * (this.discount / 100));
+        if (this.discountType === 'amt') return totalValue - this.discountValue;
+        if (this.discountType === 'percent') return totalValue - (totalValue * (this.discountValue / 100));
       }
-      return a;
+      return totalValue;
     }
   }
 });
--- src/api/billing/billing.model.js
+++ src/api/billing/billing.model.js
@@ -1,19 +1,28 @@
 const Datatypes = require('sequelize');
 const BillingItem = require('./billing-items.model');
 const Patient = require('../patient/patient.model');
-const Clinic = require('../institution/institution.model');
+const Institution = require('../institution/institution.model');
+const User = require('../user/user/user.model');
+const Profile = require('../profile/profile.model');
 
 const Billing = sequelize.define('billing', {
   id: { type: Datatypes.UUID, defaultValue: Datatypes.UUIDV1, primaryKey: true },
   status: { type: Datatypes.STRING, allowNull: false, defaultValue: 'unpaid' }
 }, {
   defaultScope: {
-    include: ['billingItems', 'patient', 'clinic']
+    include: [
+      'billingItems',
+      { model: User, as: 'physician', attributes: ['email'], include: [{ model: Profile, attributes: ['firstName', 'lastName', 'fullName', 'title'] }] },
+      { model: Patient, as: 'patient', attributes: ['fullName', 'firstName', 'lastName', 'mailingAddress'] },
+      { model: Institution, as: 'institution', attributes: ['registeredName', 'mailingAddress'] }
+    ]
   }
 });
 
 Billing.hasMany(BillingItem, { as: 'billingItems' });
 Billing.belongsTo(Patient, { as: 'patient' })
-Billing.belongsTo(Clinic, { as: 'clinic' });
+Billing.belongsTo(Institution, { as: 'institution' });
+Billing.belongsTo(User, { as: 'physician', foreignKey: 'physicianId' })
+
 
 module.exports = Billing;
