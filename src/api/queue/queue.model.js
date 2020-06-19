af635736f6ef35f841657d340994bce5edd97e28
Anthony <anthonyballugjr@gmail.com>
2020-06-19 20:20:33 +0800

    IMPROVE:Add reference: physicianId, institutionId

--- src/api/queue/queue.model.js
+++ src/api/queue/queue.model.js
@@ -2,11 +2,13 @@ const Sequelize = require('sequelize');
 const { Op } = require('sequelize');
 const Patient = require('../patient/patient.model');
 const moment = require('moment');
+const User = require('../user/user/user.model');
+const Institution = require('../institution/institution.model');
 
 const Queue = sequelize.define('queue', {
   id: { type: Sequelize.UUID, allowNull: false, defaultValue: Sequelize.UUIDV1, primaryKey: true },
-  physicianId: { type: Sequelize.UUID, },
-  institutionId: { type: Sequelize.UUID },
+  physicianId: { type: Sequelize.UUID, references: { model: User, key: 'id' } },
+  institutionId: { type: Sequelize.UUID, references: { model: Institution, key: 'id' } },
   date: { type: Sequelize.DATEONLY, allowNull: false },
   queueNumber: { type: Sequelize.INTEGER, allowNull: false },
   type: { type: Sequelize.STRING, allowNull: false, validate: { isIn: { args: [['appointment', 'walk-in']], msg: 'That is not a valid type' } } },
@@ -25,6 +27,5 @@ const Queue = sequelize.define('queue', {
 
 Queue.belongsTo(Patient);
 Patient.hasMany(Queue);
-
 
 module.exports = Queue;
