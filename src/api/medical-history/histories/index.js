0ec30a77d8f2afeeeecb3f4524f33fd000bcf3ed
Anthony <anthonyballugjr@gmail.com>
2020-07-01 17:11:36 +0800

    IMPROVE: Update SPH, OBH models

--- src/api/medical-history/histories/index.js
+++ src/api/medical-history/histories/index.js
@@ -1,27 +1,56 @@
 const Sequelize = require('sequelize');
-const { Allergy, PastIllness, Medication, Substance, Surgery } = require('../sub-histories');
+const { Allergy, Medication, Substance, Surgery, MedicalCondition } = require('../sub-histories');
 
 const PastMedicalHistory = sequelize.define('pastMedicalHistory', {
 }, {
   freezeTableName: true,
-  defaultScope: { include: [Allergy, PastIllness, Medication, Surgery] }
+  defaultScope: { include: [Allergy, Medication, Surgery, MedicalCondition] }
 });
 
 PastMedicalHistory.hasMany(Allergy);
-PastMedicalHistory.hasMany(PastIllness, { foreignKey: 'parentId', scope: { parent: 'pmh' } });
+PastMedicalHistory.hasMany(MedicalCondition, { foreignKey: 'parentId', scope: { parent: 'pmh' } });
 PastMedicalHistory.hasMany(Medication);
 PastMedicalHistory.hasMany(Surgery);
 
 const FamilyMedicalHistory = sequelize.define('familyMedicalHistory', {
 }, {
   freezeTableName: true,
-  defaultScope: { include: [PastIllness] }
+  defaultScope: { include: [MedicalCondition] }
 });
 
-FamilyMedicalHistory.hasMany(PastIllness, { foreignKey: 'parentId', scope: { parent: 'fmh' } });
+FamilyMedicalHistory.hasMany(MedicalCondition, { foreignKey: 'parentId', scope: { parent: 'fmh' } });
 
 const SocialPersonalHistory = sequelize.define('socialPersonalHistory', {
-  lifestyle: Sequelize.STRING
+  educationalAttainment: Sequelize.STRING,
+  hcp: { type: Sequelize.STRING, comment: 'Health Condition of Partner' },
+  natureOfWork: Sequelize.STRING,
+  nheh: { type: Sequelize.STRING, comment: 'No of Hours Exposed to Hazards' },
+  smu: { type: Sequelize.STRING, comment: 'Safety Measures Used' },
+  irfr: { type: Sequelize.STRING, comment: 'Interpersonal Relationship and Financial Resources' },
+  sourceOfWater: Sequelize.STRING,
+  wasteDisposal: Sequelize.STRING,
+  travelHistory: Sequelize.TEXT,
+  srp: { type: Sequelize.STRING, comment: 'Sleep and Rest Pattern' },
+  ne: { type: Sequelize.STRING, comment: 'Nutrition and Elimination' },
+  activities: Sequelize.STRING,
+  exercise: Sequelize.STRING,
+  alcoholIntake: { type: Sequelize.STRING, defaultValue: 'Not Asked' },
+  alcoholIntakeRemarks: Sequelize.TEXT,
+  caffeineIntake: { type: Sequelize.STRING, defaultValue: 'Not Asked' },
+  caffeineIntakeRemarks: Sequelize.TEXT,
+  tobaccoIntake: { type: Sequelize.STRING, defaultValue: 'Not Asked' },
+  tobaccoIntakeRemarks: Sequelize.TEXT,
+  recreationalDrugIntake: { type: Sequelize.STRING, defaultValue: 'Not Asked' },
+  recreationalDrugIntakeRemarks: Sequelize.TEXT,
+  supplementIntake: { type: Sequelize.STRING, defaultValue: 'Not Asked' },
+  supplementIntakeRemarks: Sequelize.TEXT,
+  otherIntake: { type: Sequelize.STRING, defaultValue: 'Not Asked' },
+  otherIntakeRemarks: Sequelize.TEXT,
+  isSexuallyActive: { type: Sequelize.STRING, defaultValue: 'Not Asked' },
+  isSexuallyActiveRemarks: Sequelize.TEXT,
+  hadSti: { type: Sequelize.STRING, comment: 'STI = Sexually Transitted Disease', defaultValue: 'Not Asked' },
+  hadStiRemarks: Sequelize.TEXT,
+  nvp: { type: Sequelize.TEXT, comment: 'Number and Variety of Partners' }
 }, {
   freezeTableName: true,
   defaultScope: { include: [Substance] }
@@ -31,18 +60,26 @@ SocialPersonalHistory.hasMany(Substance);
 
 const ObGyneHistory = sequelize.define('obGyneHistory', {
   unremarkable: { type: Sequelize.BOOLEAN, defaultValue: false },
-  referringOB: Sequelize.STRING,
   gravidity: Sequelize.INTEGER,
-  fullTermPregnancy: Sequelize.INTEGER,
-  noOfMiscarriage: Sequelize.INTEGER,
-  ageOfFirstMenstruation: Sequelize.INTEGER,
-  lastMenstruationPeriod: Sequelize.DATEONLY,
   parity: Sequelize.INTEGER,
+  termPregnancy: Sequelize.INTEGER,
   noOfAbortion: Sequelize.INTEGER,
   noOfLivingChildren: Sequelize.INTEGER,
-  periodDuration: Sequelize.INTEGER,
-  lastPapsmearDate: Sequelize.DATEONLY,
-  lastPapsmearRemarks: Sequelize.STRING
+  menarche: Sequelize.STRING,
+  interval: Sequelize.STRING,
+  duration: Sequelize.STRING,
+  amount: Sequelize.STRING,
+  symptoms: Sequelize.STRING,
+  contraception: Sequelize.STRING,
+  lastPapsmear: Sequelize.STRING,
+  lastMenstruationPeriod: Sequelize.DATEONLY,
+  previousMenstruationPeriod: Sequelize.DATEONLY,
+  AOGbyLMP: { type: Sequelize.DATEONLY, comment: 'current date - last menstruation period (by weeks)' },
+  lastUltrasoundDate: Sequelize.DATEONLY,
+  AOGinLastUltrasoundW: Sequelize.INTEGER,
+  AOGinLastUltraSoundD: Sequelize.INTEGER,
+  AOGbyUTZ: { type: Sequelize.DATEONLY, comment: 'current date - last ultrasound date' },
+  EDDfromLMP: { type: Sequelize.DATEONLY, comment: 'last menstruation period + 280 days' }
 }, {
   freezeTableName: true,
   setterMethods: {
