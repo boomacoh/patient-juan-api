3c4d04b459571c683fb307e9a9841cc5c2e905aa
Anthony <anthonyballugjr@gmail.com>
2020-07-21 10:07:56 +0800

    IMPROVE: Add setterMethods for physical exams

--- src/api/physical-exam/physical-exam.model.js
+++ src/api/physical-exam/physical-exam.model.js
@@ -6,7 +6,7 @@ const PhysicalExam = sequelize.define('physicalExam', {
   chestAndLungs: Sequelize.STRING,
   cardiovascular: Sequelize.STRING,
   abdomen: Sequelize.STRING,
-  skinBackExtremeties: Sequelize.STRING,
+  skinBackExtremities: Sequelize.STRING,
   nervousSystem: Sequelize.STRING,
   rectalExamination: Sequelize.STRING,
   pelvicExamination: Sequelize.STRING,
@@ -23,6 +23,45 @@ const PhysicalExam = sequelize.define('physicalExam', {
   adHeight: Sequelize.STRING,
   adWeight: Sequelize.STRING,
   adRemarks: Sequelize.STRING,
+}, {
+  setterMethods: {
+    generalSurvey(value) {
+      if (!value) return this.setDataValue('generalSurvey', 'Not Done');
+      this.setDataValue('generalSurvey', value);
+    },
+    heent(value) {
+      if (!value) return this.setDataValue('heent', 'Not Done');
+      this.setDataValue('heent', value);
+    },
+    chestAndLungs(value) {
+      if (!value) return this.setDataValue('chestAndLungs', 'Not Done');
+      this.setDataValue('chestAndLungs', value);
+    },
+    cardiovascular(value) {
+      if (!value) return this.setDataValue('cardiovascular', 'Not Done');
+      this.setDataValue('cardiovascular', value);
+    },
+    abdomen(value) {
+      if (!value) return this.setDataValue('abdomen', 'Not Done');
+      this.setDataValue('abdomen', value);
+    },
+    skinBackExtremities(value) {
+      if (!value) return this.setDataValue('skinBackExtremities', 'Not Done');
+      this.setDataValue('skinBackExtremities', value);
+    },
+    nervousSystem(value) {
+      if (!value) return this.setDataValue('nervousSystem', 'Not Done');
+      this.setDataValue('nervousSystem', value);
+    },
+    rectalExamination(value) {
+      if (!value) return this.setDataValue('rectalExamination', 'Not Done');
+      this.setDataValue('rectalExamination', value);
+    },
+    pelvicExamination(value) {
+      if (!value) return this.setDataValue('pelvicExamination', 'Not Done');
+      this.setDataValue('pelvicExamination', value);
+    },
+  }
 });
 
 
